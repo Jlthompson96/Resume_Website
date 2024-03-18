57532d3f4770926c361b8b547cac32f390f971ba
Joey Thompson <joeythompson1014@gmail.com>
2024-03-18 11:18:19 -0400

    Structure update

---
+++ src/components/HeroBanner.tsx
@@ -0,0 +1,11 @@
+
+const HeroBanner = () => {
+    return (
+        <div>
+            <h1>Hero Banner</h1>
+            <p>Hero banner goes here</p>
+        </div>
+    );
+}
+
+export default HeroBanner;
---
+++ src/components/WorkExperience.tsx
@@ -0,0 +1,11 @@
+
+const WorkExperience = () => {
+    return (
+        <div>
+            <h1>Work Experience</h1>
+            <p>Work experience goes here</p>
+        </div>
+    );
+}
+
+export default WorkExperience;
