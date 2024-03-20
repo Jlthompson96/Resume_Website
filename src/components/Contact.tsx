751ee3307395c768ccef9dcc2d8f29e5c3ebd5ee
Joey Thompson <joeythompson1014@gmail.com>
2024-03-19 20:30:47 -0400

    Contact update

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,6 +2,7 @@ import { Container, Paper, Typography } from "@mui/material";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import './Contact.scss';
 
 const Contact = () => {
 
@@ -10,9 +11,15 @@ const Contact = () => {
             <Paper elevation={3} sx={{ padding: '20px', marginTop: '50px' }}>
                 <Typography variant="h4" sx={{ fontWeight: 'bold' }} gutterBottom> Contact Me </Typography>
                 <div style={{ display: 'flex', justifyContent: 'space-between', padding: '20px' }}>
-                    <GitHubIcon sx={{ height: '10%', width: '10%' }} />
-                    <EmailIcon sx={{ height: '10%', width: '10%', color: '#999999' }} />
-                    <LinkedInIcon sx={{ height: '10%', width: '10%', color: '#0077B5' }} />
+                    <a href="https://github.com/jlthompson96">
+                        <GitHubIcon className="gh-icon" />
+                    </a>
+                    <a href="mailto:joeythompson1014@gmail.com">
+                        <EmailIcon className="email-icon" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/joseph-l-thompson/">
+                        <LinkedInIcon className="linkedin-icon" />
+                    </a>
                 </div>
             </Paper>
         </Container >
